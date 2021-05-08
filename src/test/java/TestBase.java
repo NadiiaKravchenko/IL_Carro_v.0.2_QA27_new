452e370b54496c281e82b1d60914b05ac016a846
NadiiaKravchenko <nabinkra@gmail.com>
2021-05-08 08:57:09 +0300

    added TetBase

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    WebDriver wd;
+
+    @BeforeSuite
+    public void init() {
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.navigate().to("https://ilcarro.xyz/search");
+
+    }
+
+    @AfterSuite
+    public void teardown() {
+        wd.quit();
+    }
+}
