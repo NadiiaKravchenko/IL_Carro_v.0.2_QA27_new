09157f3d45c843b5585f192d7e3a9705ae7b1415
NadiiaKravchenko <nabinkra@gmail.com>
2021-05-17 22:23:42 +0300

    Added New Car

--- src/test/java/CreatNewCarTest.java
+++ src/test/java/CreatNewCarTest.java
@@ -1,3 +1,4 @@
+import models.Car;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -5,12 +6,39 @@ import org.testng.annotations.Test;
 public class CreatNewCarTest extends TestBase {
     @BeforeMethod
     public void precondition() {
+            if (!app.user().isLogged()) {
+                app.user().loggin();
+            }
 
     }
 
     @Test
     public void createNewCar() {
-   //     app.car().opencarCreationForm();
+        Car car = new Car().withLocation("Tel-Aviv")
+                .withMake("Subaru")
+                .withModel("Forester")
+                .withYear("2017")
+                .withEngine("2000")
+                .withFuel("Petrol")
+                .withGear("AT")
+                .withWd("RWD")
+                .withDoors("5")
+                .withSeats("5")
+                .withCarClass("Luxury")
+                .withFuelConsumption("10")
+                .withCarRegNum("8511432")
+                .withPrice("350")
+                .withDistanceIncluded("500");
+
+
+
+        app.car().opencarCreationForm();
+        app.car().fillCarForm(car);
+        app.car().attachFoto("C:\\Nadiia\\IT\\QA_Automation_Lessons\\GitHub_QA27\\IL_Carro_v.0.2_QA27_new\\Subaru.jpg");
+        app.car().pause(2000);
+        app.car().saveNewCar();
+
+
 
 
     }
